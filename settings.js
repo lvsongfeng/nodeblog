ca79079761662e2debbdeffda49a9bb497b75604
lvsongfeng <songfeng.lv@zhaopin.com.cn>
2017-08-01 19:16:43 +0800

    提交node blog

---
+++ settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+    cookieSecret: 'blogkey', //用于 Cookie 加密与数据库无关
+    db: 'nodeblog', //数据库的名称
+    host: '127.0.0.1', //数据库的地址
+    port: 27017,  //数据库的端口号
+    url: "mongodb://127.0.0.1:27017/nodeblog"
+}
